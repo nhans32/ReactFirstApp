2727cc90ca5c00e9a8f8f5b2080f2321adec7cca
nhans32 <56236772+nhans32@users.noreply.github.com>
2021-01-10 17:23:53 -0800

    updated with delete buttons

--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,3 @@
-import React, { Component } from 'react'
-
 const TableHeader = () => {
     return (
       <thead>
@@ -14,27 +12,28 @@ const TableHeader = () => {
   const TableBody = props => {
     const rows = props.characterData.map((row, index) => {
       return (
-        <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-        </tr>
+<tr key={index}>
+  <td>{row.name}</td>
+  <td>{row.job}</td>
+  <td>
+    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+  </td>
+</tr>
       )
     })
   
     return <tbody>{rows}</tbody>
   }
 
-  class Table extends Component {
-    render() {
-      const { characterData } = this.props
+  const Table = props => {
+    const { characterData, removeCharacter } = props
   
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={characterData} />
-        </table>
-      )
-    }
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+      </table>
+    )
   }
 
 export default Table
