fbcd24dc01275a77c6a9437cc7347035228be1b5
nhans32 <56236772+nhans32@users.noreply.github.com>
2021-01-13 14:54:13 -0800

    update POST code 201

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Table from './Table'
 import Form from './Form';
+import axios from 'axios';
 
 class App extends Component {
   state = {
@@ -18,7 +19,35 @@ class App extends Component {
   }
 
   handleSubmit = character => {
-    this.setState({ characters: [...this.state.characters, character] })
+    this.makePostCall(character).then( callResult => {
+       if (callResult === true) {
+          this.setState({ characters: [...this.state.characters, character] });
+       }
+    });
+  }
+
+  makePostCall(character){
+    return axios.post('http://localhost:5000/users', character)
+     .then(function (response) {
+       console.log(response);
+       return (response.status === 201);
+     })
+     .catch(function (error) {
+       console.log(error);
+       return false;
+     });
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/users')
+    .then(res => {
+      const characters = res.data.users_list;
+      this.setState({ characters });
+    })
+    .catch(function (error) {
+      //Not handling the error. Just logging into the console.
+      console.log(error);
+    })
   }
 
   render() {
